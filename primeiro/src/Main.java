2719cd7ecf50d6247761461d2808540f75f730b3
Daniel Jorge Manzano <djorgemanzano@gmail.com>
2025-01-08 12:14:42 -0300

    comecando a aprende essa bomba

---
+++ primeiro/src/Main.java
@@ -0,0 +1,12 @@
+public class Main{
+    public static void main(String[] args) {
+        System.out.println("começando nessa praga de java.\nmisericordia o bagulho é nervoso");
+        int bosta = 2;
+        System.out.println("oi " + bosta + " oie");
+        String str = "teste\nteste";
+        System.out.println(str);
+        char salmonela = 'A';
+        bosta = salmonela;
+        System.out.println("valor de salmonela como ascii: " + bosta);
+    }
+}
