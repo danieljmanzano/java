86ceb7c2d0e6f26c3b5be92ef36c1e9b9a9bda15
Daniel Jorge Manzano <djorgemanzano@gmail.com>
2025-01-10 12:43:51 -0300

    arrumei uns negocio desnecessario que eu tava comitando

---
+++ netflixo/src/br/com/netflixo/modelos/Filme.java
@@ -0,0 +1,57 @@
+package br.com.netflixo.modelos;
+
+import java.util.Scanner;
+
+public class Filme {
+    private String nome;
+    private int anoDeLancamento, duracao, numNotas, somaDeNotas;
+    private float notaMedia;
+    private boolean inclusoNoPlano;
+
+
+    public void printaFicha() {
+        System.out.println("\n---- " + nome + " ----");
+        System.out.println("lançado em " + anoDeLancamento);
+        int horas = duracao / 60;
+        int minutos = duracao % 60;
+        System.out.println("duração de " + horas + "h" + minutos + "min");
+        System.out.printf("nota média: %.2f. avaliado por %d usuários\n", notaMedia, numNotas);
+        if (inclusoNoPlano)
+            System.out.println("disponível para assistir!");
+        else
+            System.out.println("não incluso no seu plano!");
+
+        for (int i = 0; i < nome.length() + 10; i++) //isso aqui é só bobeira pra printar o numero de "-" certo pra fica legal
+            System.out.print("-");
+        System.out.println();
+    }
+
+    public void setInfoTerminal() { //fiz esse set pra eu usar quando tiver preguiça de ficar colocando os dados no codigo
+        Scanner leitura = new Scanner(System.in); //vou colocar os dados como input pelo terminal
+
+        System.out.println("insira as informações referentes ao filme a ser cadastrado no sistema");
+        System.out.print("nome: ");
+        nome = leitura.nextLine();
+        System.out.print("ano de lançamento: ");
+        anoDeLancamento = leitura.nextInt();
+        System.out.print("duração do filme (em minutos): ");
+        duracao = leitura.nextInt();
+        System.out.print("o filme está incluso no plano? (1/0) ");
+        int noPlano = leitura.nextInt();
+        inclusoNoPlano = (noPlano == 1);
+    }
+
+    public void setInfoCodigo(String nome, int anoDeLancamento, int duracao, boolean inclusoNoPlano) { //esse aqui é pra se eu quiser deixar setado no codigo
+        this.nome = nome;
+        this.anoDeLancamento = anoDeLancamento;
+        this.duracao = duracao;
+        this.inclusoNoPlano = inclusoNoPlano;
+    }
+
+    public void avalia(int nota) {
+        somaDeNotas += nota;
+        numNotas++;
+        notaMedia = (float) somaDeNotas / numNotas;
+    }
+
+}
--- netflixo/src/principal.java
+++ netflixo/src/principal.java
@@ -1,10 +1,14 @@
+import br.com.netflixo.modelos.Filme;
+
 public class principal {
     public static void main(String[] args) {
         Filme f = new Filme();
-        f.recebeInfo();
+        f.setInfoTerminal();
         f.avalia(10);
         f.avalia(7);
         f.avalia(8);
+        f.printaFicha();
+        f.setInfoCodigo("filme do pelé", 1990, 100, true);
         f.printaFicha();
     }
 }
